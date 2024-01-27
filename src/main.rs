77de4fa55169ba85aaeac486f6b3c0cd0b244664
acyanbird <acyanbird@gmail.com>
2024-01-27 18:02:30 +0000

    f

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-#![no_std]
-#![no_main]
+#![no_std]  // disable the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
-#[no_mangle]
-fn _start() {
+#[no_mangle]    // don't mangle the name of this function
+pub extern "C" fn _start() {
     loop {}
 }
 
-#[panic_handler]
+#[panic_handler]    // this function is called on panic
 fn panic(_info: &core::panic::PanicInfo) -> ! {
+    // the `!` type means "this function never returns"
+    // place holder for now, we'll write this function later
     loop {}
 }
