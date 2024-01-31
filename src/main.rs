0a9ef0ca7340182ce5b8b99111ed866b23e36f7b
acyanbird <acyanbird@gmail.com>
2024-01-31 19:29:31 +0000

    lab13

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 #![no_std]  // disable the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+const BACKGROUND_COLOR: u16 = 0x1000; // blue background, black foreground
+
 #[no_mangle]    // don't mangle the name of this function
 pub extern "C" fn _start() {
+
+    let vga_buffer = unsafe { core::slice::from_raw_parts_mut(0xb8000 as *mut u16, 2000) };
+
+    for i in 0..(BUFFER_HEIGHT * BUFFER_WIDTH) {
+        vga_buffer[i] = BACKGROUND_COLOR;
+    }
+
     loop {}
 }
 
