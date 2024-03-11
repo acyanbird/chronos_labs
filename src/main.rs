7872b805b20e0d6fb8332c0137b52469f6a98e62
acyanbird <acyanbird@gmail.com>
2024-03-11 16:37:38 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@
 use chronos_labs::WRITER;
 use core::fmt::Write;
 use x86_64::registers::control::Cr3;
+use bootloader::{BootInfo, entry_point};
 
+entry_point!(kernel);
 
 #[no_mangle]    // don't mangle the name of this function
-pub extern "C" fn _start() -> !{
+fn kernel(boot_info: &'static BootInfo) -> !{
     let (level_4_page_table, _) = Cr3::read();
     writeln!(WRITER.lock(),
         "Level 4 page table at: {:?}",
