54a7443e4d76318b783f714a5e4484de5b3c38d5
acyanbird <acyanbird@gmail.com>
2024-03-24 12:22:53 +0000

    init

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,18 @@ use bootloader::{BootInfo, entry_point};
 use x86_64::structures::paging::PageTable;
 use x86_64::VirtAddr;
 
-const DISPLAY_ENTRY: i32 = 1;
-
 entry_point!(kernel);
 
 #[no_mangle]    // don't mangle the name of this function
-fn kernel(boot_info: &'static BootInfo) -> !{
+fn kernel(boot_info: &'static BootInfo) -> ! {
     let (level_4_page_table, _) = Cr3::read();
     writeln!(WRITER.lock(),
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address()
+             "Level 4 page table at: {:?}",
+             level_4_page_table.start_address()
     ).unwrap();
 
 
-
-
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-
-    // counter
-    let mut l4_counter = 0;
-    let mut l3_counter = 0;
-    let mut l2_counter = 0;
-    let mut l1_counter = 0;
-
     let (l4_entry, _) = Cr3::read();
 
     let phys = l4_entry.start_address();
@@ -38,53 +27,9 @@ fn kernel(boot_info: &'static BootInfo) -> !{
     let l4_ptr: *mut PageTable = virt.as_mut_ptr();
     let l4_table = unsafe { &*l4_ptr };
     for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() && l4_counter < DISPLAY_ENTRY {
+        if !entry.is_unused() {
             writeln!(WRITER.lock(), "L4 Entry {}: {:?}", i, entry).unwrap();
-            l4_counter += 1;
-
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            // print non-empty entries of the level 3 table
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() && l3_counter < DISPLAY_ENTRY {
-                    writeln!(WRITER.lock(), "   L3 Entry {}: {:?}", i, entry).unwrap();
-                    l3_counter += 1;
-
-                    let phys = entry.frame().unwrap().start_address();
-                    let virt = phys.as_u64() + boot_info.physical_memory_offset;
-                    let ptr = VirtAddr::new(virt).as_mut_ptr();
-                    let l2_table: &PageTable = unsafe { &*ptr };
-
-                    // print non-empty entries of the level 2 table
-                    for (i, entry) in l2_table.iter().enumerate() {
-                        if !entry.is_unused() && l2_counter < DISPLAY_ENTRY {
-                            writeln!(WRITER.lock(), "    L2 Entry {}: {:?}", i, entry).unwrap();
-                            l2_counter += 1;
-
-                            let phys = entry.frame().unwrap().start_address();
-                            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-                            let ptr = VirtAddr::new(virt).as_mut_ptr();
-                            let l1_table: &PageTable = unsafe { &*ptr };
-
-                            // print non-empty entries of the level 1 table
-                            for (i, entry) in l1_table.iter().enumerate() {
-                                if !entry.is_unused() && l1_counter < DISPLAY_ENTRY {
-                                    writeln!(WRITER.lock(), "      L1 Entry {}: {:?}", i, entry).unwrap();
-                                    l1_counter += 1;
-                                }
-                            }
-                            l1_counter = 0;
-                        }
-                    }
-                    l2_counter = 0;
-                }
-            }
-            l3_counter = 0;
         }
-
     }
 
     loop {}
