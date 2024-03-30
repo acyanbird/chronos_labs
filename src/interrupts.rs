08fabcc063a003e3bc53c0a31496cb681d120010
acyanbird <acyanbird@gmail.com>
2024-03-30 23:52:17 +0000

    update

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -7,6 +7,8 @@ use pic8259::ChainedPics;
 use x86_64::instructions::port::Port;
 
 static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+static NUMLOCK: Mutex<bool> = Mutex::new(false);
+
 
 pub fn init_idt() {
     unsafe {
@@ -14,20 +16,20 @@ pub fn init_idt() {
         IDT.load();
         IDT[0].set_handler_fn(timer);
         IDT[1].set_handler_fn(keyboard);
-
     }
 }
+
 pub static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(0, 8) });
 
 
 extern "x86-interrupt" fn breakpoint(stack_frame: InterruptStackFrame)
 {
-   writeln!(WRITER.lock(),"EXCEPTION: BREAKPOINT\n{:#?}", stack_frame).unwrap();
+    writeln!(WRITER.lock(), "Break point:\n{:#?}", stack_frame).unwrap();
 }
 
 extern "x86-interrupt" fn timer(_stack_frame: InterruptStackFrame)
 {
-    write!(WRITER.lock(),"_").unwrap();
+    write!(WRITER.lock(), "_").unwrap();
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(0);
@@ -39,24 +41,54 @@ extern "x86-interrupt" fn keyboard(_stack_frame: InterruptStackFrame)
     let mut port = Port::new(0x60);
     let scancode: u8 = unsafe { port.read() };
     let mut key: Option<&str> = None;
-    key = match scancode {
-                0x02 | 0x4F => Some("1"),
-                0x03 | 0x50 => Some("2"),
-                0x04 | 0x51 => Some("3"),
-                0x05 | 0x4B => Some("4"),
-                0x06 | 0x4C => Some("5"),
-                0x07 | 0x4D => Some("6"),
-                0x08 | 0x47 => Some("7"),
-                0x09 | 0x48 => Some("8"),
-                0x0a | 0x49 => Some("9"),
-                0x0b | 0x52 => Some("0"),
-                0x53 => Some("."),
-                _ => None,
-            };
+    let mut numlock = NUMLOCK.lock();
+    if scancode == 0x45 {
+        *numlock = !*numlock;
+        key = Some("numlock");
+    } else if !*numlock {
+        key = match scancode {
+            0x02 | 0x4F => Some("1"),
+            0x03 | 0x50 => Some("2"),
+            0x04 | 0x51 => Some("3"),
+            0x05 | 0x4B => Some("4"),
+            0x06 | 0x4C => Some("5"),
+            0x07 | 0x4D => Some("6"),
+            0x08 | 0x47 => Some("7"),
+            0x09 | 0x48 => Some("8"),
+            0x0a | 0x49 => Some("9"),
+            0x0b | 0x52 => Some("0"),
+            0x53 => Some("."),
+            _ => None,
+        };
+    } else {
+        key = match scancode {
+            0x02 => Some("1"),
+            0x03 => Some("2"),
+            0x04 => Some("3"),
+            0x05 => Some("4"),
+            0x06 | 0x4C => Some("5"),
+            0x07 => Some("6"),
+            0x08 => Some("7"),
+            0x09 => Some("8"),
+            0x0a => Some("9"),
+            0x0b => Some("0"),
+            0x4F => Some("end"),
+            0x50 => Some("DownArrow"),
+            0x51 => Some("PageDown"),
+            0x4B => Some("LeftArrow"),
+            0x4D => Some("RightArrow"),
+            0x47 => Some("home"),
+            0x48 => Some("UpArrow"),
+            0x49 => Some("PageUp"),
+            0x52 => Some("Insert"),
+            0x53 => Some("Delete"),
+            _ => None,
+        };
+    }
 
 
     if let Some(key) = key {
-        write!(WRITER.lock(),"{}", key);
+        write!(WRITER.lock(), "{}", key);
     }
 
     unsafe {
