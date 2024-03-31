2d9744a449aaf16c7e2906dcb383e43c72615a58
acyanbird <acyanbird@gmail.com>
2024-03-31 18:13:45 +0100

    update

--- tests/lab3.rs
+++ tests/lab3.rs
@@ -5,7 +5,33 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use lazy_static::lazy_static;
-use spin::Mutex;
 use chronos_labs::WRITER;
 use core::fmt::Write;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    for test in tests {
+        test();
+    }
+    writeln!(WRITER.lock(),"Running {} test(s)", tests.len()).unwrap();
+    writeln!(WRITER.lock(),"All success.").unwrap();
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    writeln!(WRITER.lock(),"Failed with error: {}", _info).unwrap();
+    loop {}
+}
